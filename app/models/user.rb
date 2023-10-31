27694a5d43ed0fa1ca195a8e1b8055a71ad33d27
naricrow <nari.crow.city@gmail.com>
2023-11-01 00:53:06 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,message: "is invalid. Include both letters and numbers"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/,message: "is invalid. Input full-width characers"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/,message: "is invalid. Input full-width characers"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー-]+\z/,message:"is invalid. Input full-width katakana characters"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー-]+\z/,message:"is invalid. Input full-width katakana characters"}
   validates :birthday, presence: true
 
   has_many :items
