3266f942508af72051f0eeb9cf578a874d941588
naricrow <nari.crow.city@gmail.com>
2023-11-07 06:19:53 +0900

    販売手数料、利益の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,13 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :content, presence: true
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, numericality: { freater_than_or_equal_to: 300, less_than_ro_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/, message: "is not a number" }
 end
