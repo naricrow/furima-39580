616b2ec3668acad547eb06e65ce6fb7d1721f96c
naricrow <nari.crow.city@gmail.com>
2023-11-06 13:20:57 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :shipping_date
+
   has_one_attached :image
+
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
