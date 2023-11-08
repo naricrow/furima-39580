277bebba3be868fd452f4230e9172605e54fc104
naricrow <nari.crow.city@gmail.com>
2023-11-08 16:01:38 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,13 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-   @items = Item.all.order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :charge_id , :area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :charge_id, :area_id, :shipping_date_id,
+                                 :price).merge(user_id: current_user.id)
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name    { 'かばん'}
-    content {Faker::Lorem.sentence}
+    name    { 'かばん' }
+    content { Faker::Lorem.sentence }
     category_id { 2 }
     condition_id { 2 }
     charge_id { 2 }
@@ -9,7 +9,7 @@ FactoryBot.define do
     shipping_date_id { 2 }
     price { 1000 }
     association :user
-  
+
     after(:build) do |item|
       item.image.attach(io: File.open('spec/fixtures/test.jpg'), filename: 'test.jpg', content_type: 'image/jpeg')
     end
