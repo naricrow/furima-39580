873253a765955312848b2e905337282470dd7f43
naricrow <nari.crow.city@gmail.com>
2023-11-22 10:46:52 +0900

    rubocop

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,26 +4,28 @@ class PurchasesController < ApplicationController
   before_action :unless_index, only: :index
   before_action :return_index, only: :index
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_shipping_address = PurchaseShippingAddress.new
   end
 
   def create
     @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
     if @purchase_shipping_address.valid?
-       pay_item
-       @purchase_shipping_address.save
-       redirect_to root_path
+      pay_item
+      @purchase_shipping_address.save
+      redirect_to root_path
     else
-       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-       render :index, status: :unprocessable_entity
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
-  
+
   def purchase_params
-    params.require(:purchase_shipping_address,).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+    params.require(:purchase_shipping_address).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def set_item
@@ -31,7 +33,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key =ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,9 +3,9 @@ class ShippingAddress < ApplicationRecord
   belongs_to :area
   belongs_to :purchase
 
-  #validates :post_code, presence: true
-  #validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  #validates :city, presence: true
-  #validates :address, presence: true
-  #validates :phone_number, presence: true
+  # validates :post_code, presence: true
+  # validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  # validates :city, presence: true
+  # validates :address, presence: true
+  # validates :phone_number, presence: true
 end
