1b114ca421f2b53994e7e88e36eaf2f121d9f237
naricrow <nari.crow.city@gmail.com>
2023-11-29 13:02:24 +0900

    Update 20231108064354_add_timestamps_to_items.rb

--- db/migrate/20231108064354_add_timestamps_to_items.rb
+++ db/migrate/20231108064354_add_timestamps_to_items.rb
@@ -1,7 +1,7 @@
 class AddTimestampsToItems < ActiveRecord::Migration[6.0]
   def change
-    add_column :items, :created_at, :datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
-    add_column :items, :updated_at, :datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
+    add_column :items, :created_at, :datetime, null: false
+    add_column :items, :updated_at, :datetime, null: false
   end
 end
 
