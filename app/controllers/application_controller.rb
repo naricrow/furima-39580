a53ef2424d68826751e78e6c23bd7e974a01e499
naricrow <nari.crow.city@gmail.com>
2023-11-30 20:24:44 +0900

    自動デプロイ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
+
 end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,26 @@
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.18.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'furima-39580'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:naricrow/furima-39580.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/naricamp名.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
